0f2cc5e12b2c6fce9a1da3eafcf74a4469e6ee69
Yannick Beynet <beynet@fresnes.info>
2013-07-31 11:41:39 +0200

    add comments.

--- src/main/java/info/yourhomecloud/network/rmi/Configuration.java
+++ src/main/java/info/yourhomecloud/network/rmi/Configuration.java
@@ -5,14 +5,17 @@ import info.yourhomecloud.configuration.HostConfigurationBean;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
-
+/**
+ * provides methos to update a remote host configuration.
+ * @author beynet
+ */
 public interface Configuration extends Remote {
     
     /**
-     * update current host configuration.This method is called folowing this sequence :
+     * update remote host configuration.This method is called folowing this sequence :
      * <ul>
-     * <li>A new host called this method on the main host</li>
-     * <li>Just after, the main host will call this method on the hosts already connected</li>
+     * <li>after connection a new host called this method on the main host exported object</li>
+     * <li>Just after, the main host will call this method on the hosts already connected to propagate changes from this new host</li>
      * </ul>
      * @param hosts
      * @return current host configuration including its own configuration
@@ -21,7 +24,7 @@ public interface Configuration extends Remote {
     public List<HostConfigurationBean> updateHosts(List<HostConfigurationBean> hosts, HostConfigurationBean newHost) throws RemoteException;
     
     /**
-     * declare that the host with provided key is exiting.
+     * declare that the host with the provided key is exiting.
      * @param hostKey
      * @throws RemoteException 
      */
