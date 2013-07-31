f8b2cf1eddc15de65870fb2912fcbf1439847ac9
Yannick Beynet <beynet@fresnes.info>
2013-07-31 11:40:31 +0200

    add comments. handling plateform file separator : string received is with "/" as a separator ; this "/" is replaced with current plateforme separator.

--- src/main/java/info/yourhomecloud/network/rmi/FileUtils.java
+++ src/main/java/info/yourhomecloud/network/rmi/FileUtils.java
@@ -5,15 +5,58 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface FileUtils extends Remote {
-    
+    /**
+     * ask to remote host to create directory specified if not already existing
+     * @param client
+     * @param rel
+     * @throws RemoteException
+     * @throws IOException 
+     */
     public void createDirectoryIfNotExist(String client,String rel) throws RemoteException,IOException ;
-    
+    /**
+     * ask to remote server if remote file exist and was not modified since specified date
+     * @param client
+     * @param rel
+     * @param millis
+     * @return
+     * @throws RemoteException
+     * @throws IOException 
+     */
     boolean isFileExistingAndNotModifiedSince(String client,String rel, long millis) throws RemoteException,IOException;
-//
+
+    /**
+     * copy specified file on the remote host
+     * @param client
+     * @param file
+     * @param modified
+     * @param rel
+     * @throws RemoteException
+     * @throws IOException 
+     */
     void copyFile(String client,byte[] file,long modified, String rel) throws RemoteException,IOException;
     
+    /**
+     * copy by chunk specified file
+     * @param client
+     * @param file
+     * @param offset
+     * @param length
+     * @param last
+     * @param modified
+     * @param rel
+     * @throws RemoteException
+     * @throws IOException 
+     */
     void copyFileByChunk(String client,byte[] file,long offset,int length,boolean last,long modified, String rel) throws RemoteException,IOException;
 
+    /**
+     * ask to remote host if specified file existsil faut 
+     * @param client
+     * @param rel
+     * @return
+     * @throws RemoteException
+     * @throws IOException 
+     */
     boolean isFileExisting(String client,String rel) throws RemoteException,IOException;
 
 //    void removeFilesRemovedOnSourceSide(String client,Path source) throws IOException;
--- src/main/java/info/yourhomecloud/network/rmi/FileUtilsImpl.java
+++ src/main/java/info/yourhomecloud/network/rmi/FileUtilsImpl.java
@@ -3,9 +3,9 @@ package info.yourhomecloud.network.rmi;
 import info.yourhomecloud.configuration.Configuration;
 import info.yourhomecloud.hosts.TargetHost;
 import info.yourhomecloud.hosts.impl.LocalTargetHost;
+import java.io.File;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.OpenOption;
@@ -13,62 +13,72 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.nio.file.attribute.FileTime;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.apache.log4j.Logger;
 
 public class FileUtilsImpl implements FileUtils {
-
+    
     private Path getTargetPathFromClient(String client) {
         return Configuration.getConfiguration().getConfigurationPath().resolve(Paths.get(client));
     }
-
+    
     private TargetHost getTargetHostFromCient(String client) {
         return new LocalTargetHost(getTargetPathFromClient(client));
     }
-
+    
+    private Path getPathFromStrPlateformIndependant(String str) {
+        if ("/".equals(File.separator)) {
+            return Paths.get(str);
+        } else {
+            str = str.replace("/", File.separator);
+            return Paths.get(str);
+        }
+    }
+    
     @Override
     public void createDirectoryIfNotExist(String client, String rel) throws IOException {
-
-        TargetHost target = getTargetHostFromCient(client);
-        Path relPath = Paths.get(rel);
-        target.createDirectoryIfNotExist(relPath);
+        try {
+            TargetHost target = getTargetHostFromCient(client);
+            Path relPath = getPathFromStrPlateformIndependant(rel);
+            target.createDirectoryIfNotExist(relPath);
+        } catch (Exception e) {
+            logger.error("error", e);
+        }
     }
-
+    
     @Override
     public boolean isFileExisting(String client, String rel) throws IOException {
         TargetHost target = getTargetHostFromCient(client);
-        Path relPath = Paths.get(rel);
+        Path relPath = getPathFromStrPlateformIndependant(rel);
         return target.isFileExisting(relPath);
     }
-
+    
     @Override
     public boolean isFileExistingAndNotModifiedSince(String client, String rel, long millis) throws IOException {
         TargetHost target = getTargetHostFromCient(client);
-        Path relPath = Paths.get(rel);
+        Path relPath = getPathFromStrPlateformIndependant(rel);
         return target.isFileExistingAndNotModifiedSince(relPath, millis);
     }
-
+    
     @Override
     public void copyFile(String client, byte[] file, long modified, String rel) throws IOException {
         Path target = getTargetPathFromClient(client);
-        Path relPath = Paths.get(rel);
+        Path relPath = getPathFromStrPlateformIndependant(rel);
         Path filePath = target.resolve(relPath);
         logger.debug("writing file " + filePath);
         Files.write(filePath, file);
         Files.setLastModifiedTime(filePath, FileTime.fromMillis(modified));
     }
-
+    
     @Override
     public void copyFileByChunk(String client, byte[] file, long offset, int length, boolean last, long modified, String rel) throws IOException {
         try {
             Path target = getTargetPathFromClient(client);
-            Path relPath = Paths.get(rel);
+            Path relPath = getPathFromStrPlateformIndependant(rel);
             Path filePath = target.resolve(relPath);
             logger.debug("writing by chunk file " + filePath.toString() + " from " + offset + " nb bytes " + length);
             final OpenOption[] options;
-
+            
             if (offset == 0) {
                 options = new OpenOption[3];
                 options[0] = StandardOpenOption.WRITE;
@@ -79,7 +89,7 @@ public class FileUtilsImpl implements FileUtils {
                 options[0] = StandardOpenOption.WRITE;
                 options[1] = StandardOpenOption.APPEND;
             }
-
+            
             try (OutputStream outputStream = Files.newOutputStream(filePath, options)) {
                 outputStream.write(file, 0, length);
             }
